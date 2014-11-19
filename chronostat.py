d7d88bcf58d64b62a14d48a8d1a57dce327b00d2
Pete Wildsmith <pete@dabapps.com>
2014-11-19 15:17:01 +0000

    don't send elapsed time if an exception was raised

--- chronostat.py
+++ chronostat.py
@@ -4,11 +4,13 @@
 chronostat.py
 ~~~~~~~~~~~~~
 
-Decorator/context manager for timing things and sending the results to StatHat.com
+Decorator/context manager for timing things and sending the results
+to StatHat.com
 
 Usage::
 
     >>> from chronostat import ChronoStat
+    >>> import time
     >>> stats = ChronoStat('pete@dabapps.com')
     >>> with stats.timer('foo'):
     ...     time.sleep(3)
@@ -23,6 +25,7 @@ from stathat import StatHat
 
 class Timer(ChronoTimer):
     """A dessert wax and a floor topping"""
+
     def __init__(self, stat, key):
         super(Timer, self).__init__()
         assert isinstance(stat, ChronoStat)
@@ -30,19 +33,22 @@ class Timer(ChronoTimer):
         self.key = key
 
     def __exit__(self, exc_type, value, tb):
-        super(Timer, self).__exit__(exc_type, value, tb)
-        self.stat.value(self.key, self.elapsed)
+        if self.elapsed:
+            self.stat.value(self.key, self.elapsed)
+        return super(Timer, self).__exit__(exc_type, value, tb)
 
     def __call__(self, f):
         @functools.wraps(f)
         def decorated(*args, **kwargs):
             with self:
                 return f(*args, **kwargs)
+
         return decorated
 
 
 class ChronoStat(StatHat):
     """Adds a timing context manager slash decorator"""
+
     def timer(self, key):
         return Timer(self, key)
 
@@ -53,4 +59,3 @@ class ChronoStat(StatHat):
             url = self.STATHAT_URL + path
             r = self.session.post(url, data=data)
             return r
-
